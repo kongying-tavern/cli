46460da156294470f3ef5425eae6b8164352d705
Alex Fang <deeno@qq.com>
2022-06-05 16:07:19 +0800

    feat: add cli callable with `version` and `help`

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+#!/usr/bin/env node
+
 import yargs from 'yargs';
 
 yargs(process.argv)
     .demandCommand(1)
+    .alias('h', 'help')
+    .alias('v', 'version')
+    .help()
+    .version()
     .parse();
